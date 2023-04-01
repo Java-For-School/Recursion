10ac4e456460cf73a5d48435c1ce7e5d248cd8de
SorerBOT <alonfiller@gmail.com>
2023-04-01 18:21:23 +0300

    (Patch) now going to be able to store long values

--- Fib/Fib.java
+++ Fib/Fib.java
@@ -2,13 +2,13 @@ public class Fib {
     
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 100; i++) {
             System.out.printf("The %d element is: %d\n", i, fib(i));
         }
     
     }
-    public static int fib(int n) {
-        int first, second, temp;
+    public static long fib(int n) {
+        long first, second, temp;
 
         first = 1;
         second = 1;
