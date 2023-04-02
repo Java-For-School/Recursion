def2e5d7490e0babca8b506ca75d5c107001cc71
SorerBOT <alonfiller@gmail.com>
2023-04-02 15:38:23 +0300

    (Feat) subSetSum

---
+++ SubSetSum/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+        System.out.println(subSetSum(arr, 5, 1));
+        System.out.println(subSetSum(arr, 5, 6));
+        System.out.println(subSetSum(arr, 5, 9));
+    }
+    private static boolean subSetSum(int[] arr, int size, int sum) {
+        if (sum == 0) return true;
+        if (size == 0) return false;
+        if (arr[size - 1] <= sum) return subSetSum(arr, size  - 1, sum - arr[size - 1]);
+
+        return subSetSum(arr, size - 1, sum);
+    }    
+}
