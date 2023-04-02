a0718276663340441198411e082c3dcfdd19fa7f
SorerBOT <alonfiller@gmail.com>
2023-04-02 15:56:50 +0300

    (Patch) subSetSum will now be more efficient

--- SubSetSum/Main.java
+++ SubSetSum/Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
     private static boolean subSetSum(int[] arr, int size, int sum) {
         if (sum == 0) return true;
-        if (size == 0) return false;
+        if (size == 0 || sum < 0) return false;
         if (arr[size - 1] <= sum) return subSetSum(arr, size  - 1, sum - arr[size - 1]);
 
         return subSetSum(arr, size - 1, sum);
